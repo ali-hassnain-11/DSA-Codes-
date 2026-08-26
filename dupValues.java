d53d2f5089107e280e88c794309614936eddecec
ali-hassnain-11 <aliiihasssnainnn@gmail.com>
2026-08-26 22:34:31 +0500

    Code completed

--- dupValues.java
+++ dupValues.java
@@ -1,7 +1,7 @@
-class q3{
+class dupValues{
 	public static void main(String[] args) {
 		
-		int[] arr = {1,2,1,3,2,4,5,3,6,5,6,5};
+		int[] arr = {1,2,3,1,2};
 
 		System.out.println(" ");
 		System.out.println("--------------------------------");
@@ -15,5 +15,38 @@ class q3{
 		System.out.print("}");		
 		System.out.println("\n------------------------------");
 		System.out.println(" ");
+
+		int[] tempArray = new int[arr.length];
+
+		int newSize = 0;
+
+		for (int i = 0;i < arr.length ;i++ ) {
+			boolean isDup = false;
+
+			for (int j = 0;j < newSize ;j++ ) {
+				if (arr[i] == tempArray[j]) {
+					isDup = true;
+					break;
+				}
+			}
+
+			if (!isDup) {
+				tempArray[newSize] = arr[i];
+				newSize++;
+			}
+		}
+
+		int[] newArray = new int[newSize];
+		for (int i = 0;i < newSize ;i++ ) {
+			newArray[i] = tempArray[i];
+		}
+		System.out.println("\n\nNew Array After Removing Duplicates:");
+
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.print(newArray[i] + " ");
+        }
+
+        System.out.println("\n\nSize of Original Array: " + arr.length);
+        System.out.println("Size of New Array: " + newArray.length);
 	}
 }
